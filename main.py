cb4bfbcabd5ab1052a23ce6a43a148cdc23bd59b
xm200 <87058640+xm200@users.noreply.github.com>
2024-12-04 23:16:00 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,46 +1,7 @@
-import yara
-import os
-import magic
-
-report = []
-
-
-def get_callback(data):
-    global report
-    report.append(data)
-    return yara.CALLBACK_CONTINUE
-
-
-filenames = os.getcwd() + '/rules/'
-path = input("Enter path to analyze: ")
-
-if not os.access(path, os.F_OK):
-    print("No such file or directory")
-    exit(0)
-
-if not os.access(path, os.R_OK):
-    print("Check the permissions and try again")
-    exit(0)
-
-if os.path.isdir(path):
-    print("Directory in uncheckable format!")
-    exit(0)
-
-
-functions = {}
-
-if magic.from_file(path, mime=True).count('text') > 0:
-    filenames += 'sources/'
-else:
-    filenames += 'binaries/'
-
-for i in os.listdir(filenames):
-    functions[i] = filenames + i
-
-rules = yara.compile(filepaths=functions)
-detected = rules.match(path, callback=get_callback, which_callbacks=yara.CALLBACK_MATCHES)
-
-if len(detected) == 0:
-    print("Congrats! No rules matched.")
-else:
-    print(*report)
+def answer(*a):
+    if type(a[0][0]) != int:
+        return "ERROR"
+    if a[0][0] % 2 == 0:
+        return "HI"
+    else:
+        return "BYE"
